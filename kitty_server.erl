177db6ad3ac2d6733e57c77133f71e6def39a719
Yuya Minami <yuya373@me.com>
2015-05-26 16:09:32 +0000

    add kitty_server

---
+++ kitty_server.erl
@@ -0,0 +1,56 @@
+-module(kitty_server).
+-export([
+         start_link/0,
+         order_cat/4,
+         return_cat/2,
+         close_shop/1
+        ]).
+-record(cat, {name, color = green, description}).
+
+% Client Api
+start_link() -> spawn_link(fun init/0).
+
+% Synchronous call
+order_cat(Pid, Name, Color, Description) ->
+  my_server:call(Pid, {order, Name, Color, Description}).
+
+% This call is asynchronous
+return_cat(Pid, Cat = #cat{}) ->
+  Pid ! {return, Cat},
+  ok.
+
+% Synchronous call
+close_shop(Pid) ->
+  my_server:call(Pid, terminate).
+
+% Server functions
+init() -> loop([]).
+
+loop(Cats) ->
+  receive
+    {Pid, Ref, {order, Name, Color, Description}} ->
+      if
+        Cats =:= [] ->
+          Pid ! {Ref, make_cat(Name, Color, Description)},
+          loop(Cats);
+        Cats =/= [] ->
+          Pid ! {Ref, hd(Cats)},
+          loop(tl(Cats))
+      end;
+    {return, Cat = #cat{}} ->
+      loop([Cat | Cats]);
+    {Pid, Ref, terminate} ->
+      Pid ! {Ref, ok},
+      terminate(Cats);
+    Unknown ->
+      io:format("Unknown message: ~p~n", [Unknown]),
+      loop(Cats)
+  end.
+
+% Private functions
+make_cat(Name, Color, Description) ->
+  #cat{name = Name, color = Color, description = Description}.
+
+terminate(Cats) ->
+  [io:format("~p was set free.~n", [C#cat.name]) || C <- Cats],
+  ok.
---
+++ my_server.erl
@@ -0,0 +1,51 @@
+-module(my_server).
+% -compile(export_all).
+-export([
+         start/2,
+         start_link/2,
+         call/2,
+         cast/2,
+         reply/2
+        ]).
+
+% Public API
+start(Module, InitialState) ->
+  spawn(fun() -> init(Module, InitialState) end).
+start_link(Module, InitialState) ->
+  spawn_link(fun() -> init(Module, InitialState) end).
+
+% Private Stuff
+
+init(Module, InitialState) ->
+  loop(Module, Module:init(InitialState)).
+
+loop(Module, State) ->
+  receive
+    {async, Msg} ->
+      loop(Module, Module:handle_cast(Msg, State));
+    {sync, Pid, Ref, Msg} ->
+      loop(Module, Module:handle_call(Msg, {Pid, Ref}, State))
+  end.
+
+% Public API
+call(Pid, Msg) ->
+  Ref = erlang:monitor(process, Pid),
+  io:format("my_server:call self(): ~p~n", [self()]),
+  Pid ! {sync, self(), Ref, Msg},
+  receive
+    {Ref, Reply} ->
+      erlang:demonitor(Ref, [flush]),
+      Reply;
+    {'DOWN', Ref, process, Pid, Reason} ->
+      erlang:error(Reason)
+  after 5000 ->
+          erlang:error(timeout)
+  end.
+
+cast(Pid, Msg) ->
+  Pid ! {async, Msg},
+  ok.
+
+reply({Pid, Ref}, Reply) ->
+  Pid ! {Ref, Reply}.
+
